838685667e6a5ae75f9bc53022e1af1360798e34
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-07-15 01:40:53 +0700

    Update and rename Constructer.cpp to Con_Destructer.cpp

--- Con_Destructer.cpp
+++ Con_Destructer.cpp
@@ -1,24 +1,37 @@
 #include<iostream>
 using namespace std;
+class message
+{
+
+};
 class woman
 {
 public: 
 	int age;
 	string name;
-// constructer
+	message* mes;
+	// constructer
 	woman(string n, int a) {
 		cout << "hello" << endl;
 		age = a;
 		name = n;
+	}
+	// destructer:  chỉ áp dụng cấp phát động 
+	~woman() {
+		cout << "delete new woman" << endl;
+		delete mes; // phải xóa vùng nhớ của những class khác trong class này( nếu đó là class động)
 	}
 	void introduce()
 	{
 		cout << " my name is " << name << endl;
 		cout << "tuoi: " << age << endl;
 	}
+	
 };
 int main() {
+	// class tĩnh tự động xóa sau mỗi ...( main...)
 	woman* hariwon = new woman("Hariwom", 12);
 
+	delete hariwon;
 	hariwon->introduce();
 }
