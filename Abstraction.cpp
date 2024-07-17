4ce9c48931590465ea2c76c6dfb9161225c733ff
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-07-17 14:41:04 +0700

    Update Abstraction.cpp

--- Abstraction.cpp
+++ Abstraction.cpp
@@ -51,7 +51,46 @@ public:
 			cout << endl;
 		}
 	}
+	int nhanVoHuong(vector<int> a, vector<int> b,int n){
+	   int t=0;
+	   for(int i=0;i<n;i++){
+	      t+= a[i]*b[i];
+	   }
+	   return t;
+	}
 	matrix nhanMaTran(matrix a, matrix b)
+	{
+	   matrix c(getRow(),getCol());
+	   if(a.col!= b.row){
+	      cout<<"khong the nhan hai ma tran!";
+	     
+	   }
+	   for(int i=0;i<b.row;i++){
+	      for(int j=0;j<b.col;j++){
+	         
+	      }
+	   }
+	   return c;
+	}
+	
+	// get hang 
+	vector<int> getRowVector(int i){
+	   if(i>=0&&i<row){
+	      return data[i];
+	   }
+	   else cout<<"row index out of range";
+	}
+	// get cot 
+	vector<int> getColVector(int j){
+	   if(j>=0&&j<data.Col){
+	      vector<int> vec(row);
+	      for(int i=0;i<row;i++){
+	         vec[i]=data[i][j];
+	      }
+	   }
+	   return vec;
+	   else cout<<"col index out of range";
+	}
 	
 };
 int main()
@@ -69,4 +108,19 @@ int main()
 	mat.setData(2, 1, 8);
 	mat.setData(2, 2, 9);
    mat.display();
+   
+   
+   matrix m(3, 3);
+
+	// Thiết lập dữ liệu cho ma trận 2
+	m.setData(0, 0, 1);
+	m.setData(0, 1, 2);
+	m.setData(0, 2, 3);
+	m.setData(1, 0, 4);
+	m.setData(1, 1, 5);
+	m.setData(1, 2, 6);
+	m.setData(2, 0, 7);
+	m.setData(2, 1, 8);
+	m.setData(2, 2, 9);
+   m.display();
 }
