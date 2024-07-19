e8d352e77da835cc1cade620e164fe87bacba7b0
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-07-19 22:52:44 +0700

    Update Abstract_Class.cpp

--- Abstract_Class.cpp
+++ Abstract_Class.cpp
@@ -1,4 +1,6 @@
 Tác dụng: quản lý các class dẫn xuất
-
+1. có ít nhất 1 pure virtual func
 2. virtual void run() = 0;
   3. 1 class dẫn xuất không override lại pure virtual function của class cơ sở ---> class đó cũng là abstract class
+
+Tất cả các hàm trong Abstract class đều là Pure virsual func ---> Interface
