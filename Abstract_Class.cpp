809f45ab9844626b59dbfd4bf4fb913069869573
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-07-19 22:49:03 +0700

    Update Abstract_Class.cpp

--- Abstract_Class.cpp
+++ Abstract_Class.cpp
@@ -1,2 +1,4 @@
-2.
+Tác dụng: quản lý các class dẫn xuất
+
+2. virtual void run() = 0;
   3. 1 class dẫn xuất không override lại pure virtual function của class cơ sở ---> class đó cũng là abstract class
